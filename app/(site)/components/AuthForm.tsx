17b6340d5a3e626bdc5850fe9e49a704a9f66286
AkramRamoul <ahmed.ramoul@univ-constantine2.dz>
2024-10-26 10:45:26 +0100

    fix : google signin

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -3,17 +3,26 @@
 import AuthSocialButton from "@/app/(site)/components/AuthSocialButton";
 import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { BsGithub, BsGoogle } from "react-icons/bs";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import axios from "axios";
 import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 type Variant = "LOGIN" | "REGISTER";
 
 function AuthForm() {
+  const session = useSession();
+  const router = useRouter();
   const [variant, setVariant] = useState<Variant>("LOGIN");
   const [isLoading, setIsloading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (session?.status === "authenticated") {
+      router.push("/users");
+    }
+  }, [session?.status, router]);
 
   const toggle_Variant = useCallback(() => {
     if (variant === "LOGIN") {
@@ -39,6 +48,7 @@ function AuthForm() {
     if (variant === "REGISTER") {
       axios
         .post("/api/register", data)
+        .then(() => signIn("credentials", data))
         .catch(() => toast.error("Something went wrong"))
         .finally(() => setIsloading(false));
     }
@@ -52,7 +62,8 @@ function AuthForm() {
             return toast.error("Invalid credentials");
           }
           if (callback?.ok && !callback.error) {
-            return toast.success("Logged in");
+            toast.success("Logged in");
+            router.push("/users");
           }
         })
         .finally(() => setIsloading(false));
