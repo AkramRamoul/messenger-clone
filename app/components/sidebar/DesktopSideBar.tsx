48a8d0fed155fb4e933a52afd3e71db59086f2a7
AkramRamoul <ahmedakramramoul@gmail.com>
2024-11-09 15:34:26 +0100

    fix :unused import removed

--- app/components/sidebar/DesktopSideBar.tsx
+++ app/components/sidebar/DesktopSideBar.tsx
@@ -1,6 +1,5 @@
 "use client";
 import useRoutes from "@/app/hooks/useRoutes";
-import { useState } from "react";
 import DesktopItem from "./DesktopItem";
 import { User } from "@prisma/client";
 
@@ -21,7 +20,6 @@ interface DesktopSideBarProps {
 }
 const DesktopSideBar: React.FC<DesktopSideBarProps> = ({ currentUser }) => {
   const routes = useRoutes();
-  const [isOpen, setIsOpen] = useState(false);
   return (
     <div
       className="
@@ -56,12 +54,7 @@ const DesktopSideBar: React.FC<DesktopSideBarProps> = ({ currentUser }) => {
         </ul>
       </nav>
       <nav className="mt-4 flex flex-col justify-between items-center">
-        <div
-          onClick={() => {
-            setIsOpen(true);
-          }}
-          className="cursor-pointer hover:opacity-75 transition"
-        >
+        <div className="cursor-pointer hover:opacity-75 transition">
           <div className="relative inline-block">
             <DropdownMenu>
               <DropdownMenuTrigger>
